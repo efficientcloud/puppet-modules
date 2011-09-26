ad57072e174321222e2ed412823eee5d3c75423a
Christian Hofstaedtler <c.hofstaedtler@efficientcloud.com>
2011-09-26 11:49:39 +0200

    ensure locales are correct

--- local/manifests/init.pp
+++ local/manifests/init.pp
@@ -19,4 +19,19 @@ class local {
 	package { "psmisc":
 		ensure => installed,
 	}
+
+	file { "/etc/locale.gen":
+		ensure => file,
+		mode => 0644,
+		owner => root,
+		group => root,
+		content => "en_US.UTF-8 UTF-8\nde_AT.UTF-8 UTF-8\n",
+		notify => Exec["fix-locales"],
+	}
+
+	exec { "fix-locales":
+		command => "locale-gen && update-locale LANG=en_US.UTF-8",
+		refreshonly => true,
+		path => "/bin:/usr/bin:/sbin:/usr/sbin",
+	}
 }
